5dc031f79e91b67c774d46dad5495b51fe6aff26
Movin Vinusandha <movinvinusandha@gmail.com>
2026-06-22 12:57:25 +0530

    test commit for auto trigger webhook

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-// ...existing code...
 
 function App() {
   const [now, setNow] = useState(() => new Date())
